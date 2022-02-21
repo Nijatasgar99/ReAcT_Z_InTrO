fa0583f559c2a86229816598e60afc0919a576ba
Nidzh__ <91114183+Nijatasgar99@users.noreply.github.com>
2022-02-22 02:32:18 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import User from './components/User';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title="Navbar"/>
+      <User name="Kamran" age="15" surname="Ismayilov"/> 
+      <User name="Kamran" age="15" surname="Ismayilov"/> 
+      <User name="Kamran" age="15" surname="Ismayilov"/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function Navbar(props) {
+  return (
+    <div>
+      <h1 className='test' style={{color:"red",backgroundColor:"teal"}}>{props.title}</h1>
+    </div>
+  )
+}
+
+export default Navbar
+  
+ 
